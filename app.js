05ef08b997f88cacaa04da312777be3874568fd2
Stephen Millard <StephenMillard@Stephens-MacBook-Pro.local>
2018-01-08 04:41:33 -0800

    upload all

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+
+
+app.use(express.static('dist'))
+
+app.get('/', function (req, res) {
+  res.render('index.html')
+})
+
+//listening port
+app.listen(3000, () => console.log('listening on port 3000'))
