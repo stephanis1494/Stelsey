6a25bcb59af49acae8cf091c4fc13412ada27b83
Stephen Millard <StephenMillard@Stephens-MacBook-Pro.local>
2018-01-08 17:07:02 -0800

    webpack update 2

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 class Test extends React.Component {
   render() {
     return(
-        <h1> Hello World! </h1>
+        <h1> Hello from React. </h1>
     );
   }
 }
