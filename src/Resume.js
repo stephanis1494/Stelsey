c482a324edf63388cb1308e290dfc977c93b345d
Stephen Fairris <stephen@stelsey.com>
2018-11-04 07:41:58 -0800

    added static template from json resume

--- src/Resume.js
+++ src/Resume.js
@@ -1,8 +1,321 @@
 import React from 'react'
-import { Navbar, Grid, Row, Col } from 'react-bootstrap'
 import './Resume.css'
 
 export const Resume = () => {
+  return(
+    <div>
+    <header id="header">
+  <div class="container">
+  <div class="row">
+  <div class="col-sm-9 col-sm-push-3">
+  <h1>
+  Ken Stipek
+  </h1>
+  <h2>
+  Web Application Developer
+  </h2>
+  </div>
+  </div>
+  </div>
+  </header>
+  <div id="content" class="container">
+  <section id="contact" class="row">
+  <aside class="col-sm-3">
+  <h3>Contact</h3>
+  </aside>
+  <div class="col-sm-9">
+  <div class="row">
+  <div class="col-sm-6">
+  <strong>Email</strong>
+  <div class="email"><a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="deb5bbb09eb5bbb0adaab7aebbb5f0bdb1b3">[email&#160;protected]</a></div>
+  </div>
+  <div class="col-sm-6">
+  <strong>Phone</strong>
+  <div class="phone">(360) 224-6691</div>
+  </div>
+  <div class="col-sm-6">
+  <strong>Website</strong>
+  <div class="website">
+  <a href="https://www.kenstipek.com">https://www.kenstipek.com</a>
+  </div>
+  </div>
+  </div>
+  </div>
+  </section>
+  <section id="about" class="row">
+  <aside class="col-sm-3">
+  <h3>About</h3>
+  </aside>
+  <div class="col-sm-9">
+  <p>Full-stack web app developer experienced in API-driven, single-page applications.</p>
+  <p>Over eight years of experience building applications from idea to deployment and continued support.</p>
+  <p>Was the first hire at Brivity, a web application targeting real estate agents; in under 18 months growing it from no revenue to a multiple products platform with over $60K in monthly recurring revenue.</p>
+  <p>Have fulfilled multiple team roles, including: team leader, engineer, operations, and support; this grants me a broad understanding of what is required in making a web product successful.</p>
+  </div>
+  </section>
+  <section id="profiles" class="row">
+  <aside class="col-sm-3">
+  <h3>Profiles</h3>
+  </aside>
+  <div class="col-sm-9">
+  <div class="row">
+  <div class="col-sm-6">
+  <strong class="network">
+  Twitter
+  </strong>
+  <div class="username">
+  <div class="url">
+  <a href="https://twitter.com/kenstipek">kenstipek</a>
+  </div>
+  </div>
+  </div>
+  <div class="col-sm-6">
+  <strong class="network">
+  GitHub
+  </strong>
+  <div class="username">
+  <div class="url">
+  <a href="https://github.com/kenstipek">kenstipek</a>
+  </div>
+  </div>
+  </div>
+  <div class="col-sm-6">
+  <strong class="network">
+  LinkedIn
+  </strong>
+  <div class="username">
+  <div class="url">
+  <a href="https://www.linkedin.com/in/ken-stipek-710ba916/">Ken Stipek</a>
+  </div>
+  </div>
+  </div>
+  <div class="col-sm-6">
+  <strong class="network">
+  Stack Overflow
+  </strong>
+  <div class="username">
+  <div class="url">
+  <a href="https://stackoverflow.com/users/1230053/ken-stipek?tab=profile">Ken Stipek</a>
+  </div>
+  </div>
+  </div>
+  </div>
+  </div>
+  </section>
+  <section id="work" class="row">
+  <aside class="col-sm-3">
+  <h3>Work</h3>
+  </aside>
+  <div class="col-sm-9">
+  <div class="row">
+  <div class="col-sm-12">
+  <h4 class="strike-through">
+  <span>Self-Employed</span>
+  <span class="date">
+  12/2016 — Present
+  </span>
+  </h4>
+  <div class="position">
+  Software Consultant
+  </div>
+  <div class="summary">
+  <p>Consulted with multiple companies, helping build their applications with React, VueJS, Ruby on Rails, and many more modern technologies.</p>
+  </div>
+  <h4>Highlights</h4>
+  <ul class="highlights">
+  <li class="bullet">TripGrid: Tripgrid is a young startup that offers logistical and travel planning tools for business teams. I designed and delivered an advanced permission system using CanCan. I also created the API, which powered the single-page application written using VueJS, and our mobile, read-only app written with React Native.</li>
+  <li class="bullet">Brivity: I helped Brivity updated one of their legacy products, namely Brivity Valuations, to be more inline with their new branding.</li>
+  </ul>
+  </div>
+  <div class="col-sm-12">
+  <h4 class="strike-through">
+  <span>AgentEcho</span>
+  <span class="date">
+  7/2016 — 12/2016
+  </span>
+  </h4>
+  <div class="position">
+  Co-Founder and CEO
+  </div>
+  <div class="summary">
+  <p>AgentEcho is a real-time reporting solution for real estate professionals. It allows users to integrate third-party real estate software and see their key performance indicators in one place.</p>
+  </div>
+  <h4>Highlights</h4>
+  <ul class="highlights">
+  <li class="bullet">My primary role was in product development. Key technologies in the stack include: Ruby on Rails 5, ReactJS, Redis, PostgresSQL, MaterializeCSS, ChartistJS, and WebSockets using Rail’s Action Cable.</li>
+  <li class="bullet">Managed documentation in Trello and Basecamp.</li>
+  </ul>
+  </div>
+  <div class="col-sm-12">
+  <h4 class="strike-through">
+  <span>Kaleo Software</span>
+  <span class="date">
+  1/2015 — 7/2016
+  </span>
+  </h4>
+  <div class="position">
+  Software Engineer
+  </div>
+  <div class="summary">
+  <p>Kaleo Software provides internal question and answer tools for large enterprises. In addition to simply providing a repository of information, Kaleo uses machine learning to best route new questions to a person who most likely knows the answer.</p>
+  </div>
+  <h4>Highlights</h4>
+  <ul class="highlights">
+  <li class="bullet">Beyond my responsibilities developing both the backend and frontend of the application, I spearheaded machine learning by leveraging IBM Watson. I was also part of the hiring and training process.</li>
+  <li class="bullet">Notable technologies in Kaleo’s stack include Ruby on Rails 3, jRuby, CoffeeScript (legacy), Elasticsearch, and ReactJS (using ES6, Redux, CSS Modules, and React-Toolkit to create a single-page app).</li>
+  </ul>
+  </div>
+  <div class="col-sm-12">
+  <h4 class="strike-through">
+  <span>Brivity</span>
+  <span class="date">
+  12/2012 — 1/2015
+  </span>
+  </h4>
+  <div class="position">
+  Technical Software Manager
+  </div>
+  <div class="summary">
+  <p>When I started at Brivity, it had just been purchased by a small group of real estate and technology business owners in Bellingham. It had fewer than a ten users. I was put in charge of developing it into a full product. While there, I helped grow it into an in million-dollar company with over a dozen employees.</p>
+  </div>
+  <h4>Highlights</h4>
+  <ul class="highlights">
+  <li class="bullet">Brivity started as a simple task manager, but grew into an industry-praised transaction management and lead generation tool.</li>
+  <li class="bullet">I lead the project and was the primary figure in development, planning, hiring, leadership, marketing, and sales.</li>
+  <li class="bullet">Brivity was built using Ruby on Rails 3, Sinatra, CoffeeScript, Google’s map API, and various other third-party tools.</li>
+  </ul>
+  </div>
+  </div>
+  </div>
+  </section>
+  <section id="volunteer" class="row">
+  <aside class="col-sm-3">
+  <h3>Volunteer</h3>
+  </aside>
+  <div class="col-sm-9">
+  <div class="row">
+  <div class="col-sm-12">
+  <h4 class="strike-through">
+  <span>Private Mentorship</span>
+  <span class="date">
+  9/2017 — Present
+  </span>
+  </h4>
+  <div class="position">
+  Mentor
+  </div>
+  <div class="summary">
+  <p>I am currently mentoring a student in technology, programming, and getting into the web industry.</p>
+  </div>
+  </div>
+  </div>
+  </div>
+  </section>
+  <section id="education" class="row">
+  <aside class="col-sm-3">
+  <h3>Education</h3>
+  </aside>
+  <div class="col-sm-9">
+  <div class="row">
+  <div class="col-sm-12">
+  <h4 class="strike-through">
+  <span>Western Washington University</span>
+  <span class="date">
+  9/2007 — 8/2009
+  </span>
+  </h4>
+  <div class="area">
+  Computer Science
+  </div>
+  <div class="studyType">
+  Bachelor of Science
+  </div>
+  </div>
+  <div class="col-sm-12">
+  <h4 class="strike-through">
+  <span>Cascadia College</span>
+  <span class="date">
+  9/2002 — 5/2004
+  </span>
+  </h4>
+  <div class="area">
+  Computer Science
+  </div>
+  <div class="studyType">
+  Associates of Science
+  </div>
+  </div>
+  </div>
+  </div>
+  </section>
+  <section id="skills" class="row">
+  <aside class="col-sm-3">
+  <h3>Skills</h3>
+  </aside>
+  <div class="col-sm-9">
+  <div class="row">
+  <div class="col-sm-6">
+  <div class="name">
+  <h4>Front-End Development</h4>
+  </div>
+  <ul class="keywords">
+  <li>Javascript</li>
+  <li>ES6</li>
+  <li>React</li>
+  <li>Redux</li>
+  <li>VueJS</li>
+  <li>Webpack</li>
+  <li>HTML</li>
+  <li>CSS</li>
+  </ul>
+  </div>
+  <div class="col-sm-6">
+  <div class="name">
+  <h4>Back-End Development</h4>
+  </div>
+  <ul class="keywords">
+  <li>Ruby on Rails</li>
+  <li>Sinatra</li>
+  <li>PostgreSQL</li>
+  <li>Elixir</li>
+  <li>Phoenix</li>
+  <li>NodeJS</li>
+  <li>Redis</li>
+  <li>Elasticsearch</li>
+  </ul>
+  </div>
+  <div class="col-sm-6">
+  <div class="name">
+  <h4>Management</h4>
+  </div>
+  <ul class="keywords">
+  <li>Lead Small and Midsized Teams</li>
+  <li>Technical and Non-Technical Teams</li>
+  <li>Public Speaking</li>
+  <li>Hiring and Interviewing</li>
+  </ul>
+  </div>
+  <div class="col-sm-6">
+  <div class="name">
+  <h4>Operations</h4>
+  </div>
+  <ul class="keywords">
+  <li>AWS EC2</li>
+  <li>AWS Lambda</li>
+  <li>AWS S3</li>
+  <li>Heroku</li>
+  <li>GIT</li>
+  </ul>
+  </div>
+  </div>
+  </div>
+  </section>
+  </div>
+  </div>
+  )
+}
+
+/* export const Resume = () => {
   return(
     <Grid>
       <Navbar id='header'>
@@ -91,4 +404,4 @@ export const Resume = () => {
       </Row>
     </Grid>
   )
-}
+} */
